8408769c44278814ea343b5200c7c915be301f34
Zidan Ramadhan <zidan.ramadhan@ui.ac.id>
2022-03-11 20:55:02 +0700

    Initial Commit

---
+++ ZNews/ZNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZNewsApp.swift
+//  ZNews
+//
+//  Created by Zidan Ramadhan on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ZNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
